8bb2b8095c4bd9ed71d0e44734caf55fb3c1ac55
Hassan <hassan.seedat07@gmail.com>
2022-07-05 12:10:41 +0100

    added in getter and setter

--- src/cardGame.java
+++ src/cardGame.java
@@ -10,6 +10,35 @@ public class cardGame {
             this.symbol = symbol;
             this.value = value;
         }
+        public String getSuit() {
+            return suit;
+        }
+
+        public void setSuit(String suit) {
+            this.suit = suit;
+        }
+
+        public String getSymbol() {
+            return symbol;
+        }
+
+        public void setSymbol(String symbol) {
+            this.symbol = symbol;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+
+
+
+
+
     }
 
 }
