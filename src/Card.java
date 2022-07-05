a9281eeaab26ea92d936cf2eac468f0841b0be27
Hassan <hassan.seedat07@gmail.com>
2022-07-05 12:20:47 +0100

    added in getter and setter

--- src/Card.java
+++ src/Card.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
 
 class CardGame {
-    String[] suits = {"\u2660", "\u2665", "\u2666", "\u2666"};
-    String[] symbols = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+    String[] suits = {"\u2660", "\u2665", "\u2665", "\u2666"};
+    String[] symbols = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
     int[] values = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
     private ArrayList<Card> deckOfCards = new ArrayList<Card>();
     private ArrayList<Card> TempDeckOfCards = new ArrayList<Card>();
