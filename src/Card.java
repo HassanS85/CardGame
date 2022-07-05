7df9a78a927984fa92764d5861c38d85050c97f1
Hassan <hassan.seedat07@gmail.com>
2022-07-05 11:45:19 +0100

    added in strings for suits and symbols - created classes

---
+++ src/Card.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+class CardGame {
+    String[] suits = {"\u2660", "\u2665", "\u2666", "\u2666"};
+    String[] symbols = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+    int[] values = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+    private ArrayList<Card> deckOfCards = new ArrayList<Card>();
+    private ArrayList<Card> TempDeckOfCards = new ArrayList<Card>();
+
+
+    public CardGame() {
+        this.deckOfCards = deckOfCards;
+        this.TempDeckOfCards = TempDeckOfCards;
+    }
+
+}
+
