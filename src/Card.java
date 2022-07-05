d94c21f337a6cc70a8fc9479486ee4df7c67eac8
Hassan <hassan.seedat07@gmail.com>
2022-07-05 13:01:19 +0100

    added in main deck and generated deck

--- src/Card.java
+++ src/Card.java
@@ -3,19 +3,12 @@
 
 import java.util.ArrayList;
 
-//Black Spade Suit (Spade) U+2660
-//Black Heart Suit (Hearts) U+2665
-//Black Diamond Suit (Diamonds) U+2666
-// Black Club Suit (Club) "U+2663"
-
-
-
 class CardGame {
     String[] suits = {"\u2660", "\u2665", "\u2666", "\u2666"};
     String[] symbols = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
     int[] values ={2,3,4,5,6,7,8,9,10,11,12,13,14};
-    private ArrayList<cardGame.Card> deckOfCards = new ArrayList<cardGame.Card>();
-    private ArrayList<cardGame.Card> TempDeckOfCards = new ArrayList<cardGame.Card>();
+    private ArrayList<Card> deckOfCards = new ArrayList<Card>();
+    private ArrayList<Card> TempDeckOfCards = new ArrayList<Card>();
 
 
     public CardGame() {
@@ -27,7 +20,7 @@ class CardGame {
 
         for (String suit: suits){
             for (int j = 0; j <symbols.length;j++){
-                cardGame.Card myCard  = new cardGame.Card(suit, symbols[j], values[j]);
+                Card myCard  = new Card(suit, symbols[j], values[j]);
                 deckOfCards.add(myCard);
                 TempDeckOfCards.add(myCard);
             }
@@ -38,7 +31,7 @@ class CardGame {
     }
 
     public void getDeck(){
-        for(cardGame.Card cards: deckOfCards){
+        for(Card cards: deckOfCards){
             System.out.println(cards);
         }
     }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
 
-
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
-        cardGame.Card card = new cardGame.Card("Spade","6",6);
+        Card card = new Card("Spade", "6", 6);
         System.out.println(card.toString());
 
         CardGame deck = new CardGame();
@@ -15,4 +12,6 @@ public class Main {
         deck.getFirstCard();
 
     }
+
 }
+
